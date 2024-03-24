e3acbabb6a26d90a8482c800fe351582fc048511
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2024-03-24 16:00:56 +0530

    strings

---
+++ PythonTutorial/data_structures/my_strings.py
@@ -0,0 +1,18 @@
+'''
+Created on 24-Mar-2024
+
+@author: admin
+'''
+a=" https://github.com/VivekNagaraju/B29PS "
+# for i in a:
+#     print(i)
+    
+# print(a.capitalize())
+# print(a.casefold())
+# print(a.lower())
+# print(a.upper())
+# print(a.strip())
+# print(a.lstrip())
+# print(a.rstrip())
+
+print(a.rsplit("/"))
