e45c5a0ee2bd2f39f6c3484e57dcbdbb82b3edac
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2024-03-31 13:44:37 +0530

    31-03-24

---
+++ PythonTutorial/oops/polymorphism.py
@@ -0,0 +1,44 @@
+'''
+Polymorphism:
+Poly= Many
+Morph= form
+
+1. Overloading:
+    a. Operator overloading
+        + -->  arithmetic addition of 2 Numerical data type objects
+          --> combining  2 strings
+          --> Extending a list with the elements of another list
+    b. Method overloading
+    c. Constructor overloading
+2. Over-riding: 
+    a. Method over-riding
+    b. Constructor over-riding
+'''
+
+'''
+# Operator overloading
+
+print(2+4.9)
+print("2" + "4.9")
+print([1, 2, 3, 4] + [5, 6, 7, 8])
+'''
+
+# Method Overloading
+
+class Example:
+    def __init__(self, *b): # constructor overloading with the help of variable length arguments
+        print(b)
+        
+    def method_one(self, *a): # method overloading with the help of variable length arguments
+        print(a)
+    
+    def method_two(self, a=0, b=0, c=0, d=0):  # method overloading using default values
+        print(a+b+c+d)  
+        
+obj1=Example(1, 2)
+obj1.method_one(1, 3, 4)
+obj1.method_one(1,3, 4, 5,6, 6, 7)
+obj1.method_two()
+obj1.method_two(1)
+obj1.method_two(2, 4)
+obj1.method_two(3, 5, 6, 7)
