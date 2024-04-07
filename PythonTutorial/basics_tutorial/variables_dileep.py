c5231bae66465c7d1ee442979aec6db44839b6b8
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2024-04-07 11:10:20 +0530

    10-03-24: Part-1

---
+++ PythonTutorial/basics_tutorial/variables_dileep.py
@@ -0,0 +1,27 @@
+'''
+Variable: Name given to the memory location where data (i/o) is stored
+
+Syntax:
+
+name of the variable = value/ data
+
+name should start with alphabet character
+
+Invalid cases:
+1. value should not be on Left Hand Side
+2. Variable name should not start with numericals
+'''
+
+a=1
+print(a)
+name="Vivek"
+print(name)
+address1=123
+print(address1)
+
+# 101=123
+# print(101)
+# 1=a
+# print(a)
+
+
